5d8791c124842fc6b36d1a11919c51ebf9812652
hamdadf <hamdadf14@gmail.com>
2019-08-09 10:18:14 +0700

    first commit

---
+++ app/src/main/java/tubes/hamda/app/running/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+package tubes.hamda.app.running;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
